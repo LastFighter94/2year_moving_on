72027b602dee5e77e0b9ae2d726223132440c856
LastFighter94 <yurykishko@yandex.ru>
2024-01-26 08:05:22 +0800

    continue learning

--- PHP/PHP_LARAVELL laravell from scratch/laragigs/routes/web.php
+++ PHP/PHP_LARAVELL laravell from scratch/laragigs/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ListingController::class, 'index']);
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
+Route::get('/', function () {
+    return view('welcome');
+});
